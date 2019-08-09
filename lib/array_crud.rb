40df4badbeff947fc75db785359fb59c81747e35
Darrin Barker <darrinp.barker@gmail.com>
2019-08-09 07:08:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  my_array=[]
 end
 
 def create_an_array
-  
+  numbers=[1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array << "arrays!"
+  p add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,7 +16,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  remove_element_from_end_of_array = ["I,am,really,learning,arrays!"]
+  arrays!=remove_element_from_start_of_array.pop
   
+  p remove_element_from_start_of_array
+  p arrays!
 end
 
 def remove_element_from_start_of_array(array)
